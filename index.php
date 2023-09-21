4317fd3b049211c4b00c3c6cba86b88c6fbb97d4
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-21 11:57:22 -0300

    Inclusão de mais  Acesso e atribuição

--- index.php
+++ index.php
@@ -6,26 +6,38 @@
     <title>Exemplo</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 1</h1>
+    <h1>PHP com POO - Exemplo 2</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importação do arquivo de classe</li>
-        <li>Criação de instâncias/objetos</li>
+        <li>Acesso direto às propriedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de método</li>        
     </ul>
 
-    <?php
-    // Importando Classe
+    <?php   
     require_once "src/Cliente.php";
+    
+    $clienteA = new Cliente;
+    $clienteB = new Cliente;
 
-    //Criando instâncias da Classe (Objetos!)
-    $ClienteA = new Cliente;
-    $ClienteB = new Cliente;
+    //Acesso e atribuição
+    $clienteA->nome ="Aline";
+    $clienteB->nome ="Rafaela";
+
+    $clienteA->telefones = ["11-2135-0300", "11-95555-5555"];
+
+    $clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
+
     ?>
+
+    <hr>
+    <h2>Dados dos objetos</h2>
+    <h3><?=$clienteA->nome?></h3>
+    <h3><?=$clienteB->nome?></h3>
     
     <pre>
-        <?=var_dump($ClienteA, $ClienteB)?>
+        <?=var_dump($clienteA, $clienteB)?>
     </pre>
 
 

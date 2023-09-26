594ac9a345023945a63dfd0040797f5d182bc236
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-26 08:57:50 -0300

    Exemplo 5 Herança

--- index.php
+++ index.php
@@ -3,45 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 04</title>
+    <title>Exemplo 05</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 4</h1>
+    <h1>PHP com POO - Exemplo 5</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Encapsulamento</li>
-        <li>Modificadores de visibilidade (public, private e protected)</li>
-        
-    </ul>
-
-    <?php   
-    require_once "src/Cliente.php";  
-    $clienteA = new Cliente;
-
-    //Código a baixo vai dar erro pois a propriedade é privada e Não pode alterar fora da classe
-    //$clienteA->nome = "Aline"; 
-
-    // Usando o setter para atribuir um valor à propriedade privada "nome"
-    $clienteA->setNome("Fulando da Silva");
-    $clienteA->setEmail("fukando@gmail");
-    $clienteA->setSenha("123abc");
-    ?>
-    
-
-    <!-- Usando o getter para ler/acessar dados privados -->
-    <p>Nome: <?=$clienteA->getNome()?></p>
-    <p>Email: <?=$clienteA->getEmail()?></p>
-    <p>Senha: <?=$clienteA->getSenha()?></p>
-
-    
-    <pre>
-        <?=var_dump($clienteA)?>       
-    </pre>
-
-    
-    
-
+        <li>Herança (Super Classe e Sub Classe)</li>        
+    </ul>     
 
     
 </body>
